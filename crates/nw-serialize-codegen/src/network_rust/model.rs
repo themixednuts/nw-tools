1f89df496fa5ab18336a420b1731d8cb11540a12
themixednuts <118081862+themixednuts@users.noreply.github.com>
2026-08-12 23:52:51 -0500

    Invalidate generated schema after emitter changes

--- crates/nw-serialize-codegen/src/network_rust/model.rs
+++ crates/nw-serialize-codegen/src/network_rust/model.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-pub const NETWORK_RUST_EMITTER_VERSION: &str = "network-rust-v65";
+pub const NETWORK_RUST_EMITTER_VERSION: &str = "network-rust-v66";
 
 #[derive(Debug, Error)]
 pub enum NetworkRustEmitError {
